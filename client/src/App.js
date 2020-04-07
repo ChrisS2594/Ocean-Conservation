5fbca95d8204d0be6c13d6d6b77e947e9edb998d
Chris <stephens-c2@live.com>
2020-04-06 20:31:58 -0700

    moving Nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <h2>Welcome to React</h2>
+          <h1> C.C.M.R.J Oceanic Conservation</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
       </div>
     );
   }

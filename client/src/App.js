24d9e7f8e93d16caf7c0e028dec87099aed5df77
Chris <stephens-c2@live.com>
2020-04-14 15:44:37 -0700

    compiling compnenets into main.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "./Components/Nav/index"
 import "./App.scss";
 import Main from "./../src/pages/main"
+import Foot from ".//Components/Footer/Foot"
 
 class App extends Component {
   render() {
@@ -10,10 +11,12 @@ class App extends Component {
         <div className="App-header">
           <h1> C.C.M.R.J Oceanic Conservation</h1>
           <Nav />
+          < Main />
+
           
 
 
-
+      <Foot />
         </div>
        
       </div>
